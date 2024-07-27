9307944476e8dd701389a20570bcde81ffe680c4
abdelkarim el manssouri <elmanssouriabdelkarim0@gmail.com>
2024-07-27 16:19:37 +0100

    carousel done

--- script.js
+++ script.js
@@ -9,3 +9,59 @@ navToggle.addEventListener("click", () => {
   prinmaryNav.toggleAttribute("data-visible");
   primaryHeader.toggleAttribute("data-overlay");
 });
+
+new Swiper(".swiper", {
+  // Optional parameters
+  effect: "coverflow",
+  coverflowEffect: {
+    slideShadows: false,
+  },
+  speed: 1000,
+  direction: "horizontal",
+  loop: true,
+  lazyLoading: true,
+  centeredSlides: true,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+
+  // default slides per view
+  slidesPerView: 3,
+  spaceBetween: 10,
+
+  breakpoints: {
+    // when window width is => 320px
+    320: {
+      slidesPerView: 1,
+      // spaceBetween: 20
+    },
+    // when window width is => 768px
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+    // when window width is => 1000px
+    1000: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+  },
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+});
